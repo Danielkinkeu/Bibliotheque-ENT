26ec4379fa916a92c6010ec0527b32a91f552744
Danielkinkeu <99888946+Danielkinkeu@users.noreply.github.com>
2023-11-22 11:08:50 +0100

    User table

--- Bibliotheque/src/main/java/com/king/Bibliotheque/BibliothequeApplication.java
+++ Bibliotheque/src/main/java/com/king/Bibliotheque/BibliothequeApplication.java
@@ -2,12 +2,9 @@ package com.king.Bibliotheque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BibliothequeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BibliothequeApplication.class, args);
 	}
-
 }
---
+++ Bibliotheque/src/main/java/com/king/Bibliotheque/User.java
@@ -0,0 +1,74 @@
+package com.king.Bibliotheque;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String subName;
+    private String email;
+    private String birthDate;
+    private String phone;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
